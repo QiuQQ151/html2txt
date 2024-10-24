a6626a7368e05682af075b466b8836e3d7107e42
JDYUN <18420171908@163.com>
2024-10-24 14:34:17 +0800

    实现指定日期抓取

--- main.c
+++ main.c
@@ -13,7 +13,7 @@ data: 日期 格式：年+月+日 例子：20241014
 int main(int argc, char *argv[]) {
     // 资源申请
     // 打开输出文件 //按日期命名
-    FILE * log_fp = fopen("./log/log.txt", "w"); //追加模式a+
+    FILE * log_fp = fopen("./log/log.txt", "w"); //覆盖模式w，追加模式a+
     if (!log_fp) {
         fprintf(stderr, "无法打开log文件 %s\n", "./log/log.txt");
         return 1;
@@ -27,32 +27,35 @@ int main(int argc, char *argv[]) {
     *time = '\0';
     *(time+8) = '\0';
 
+    // 获取当前时间
+    char* local_time = get_time();
+    log_record(local_time,log_fp);
+    log_record("\n",log_fp);
+    printf("系统时间：%s\n",local_time);   
+
     // 抓取时间设定
-    if( argc == 1 | argc == 2)// 未指定日期，抓取时间设定未系统时间
+    if( argc == 1 )// 未指定日期，抓取时间设定未系统时间
     {
         // 内容抓取并保存
         log_record("---启动内容抓取----按系统当前时间抓取:",log_fp);
-        char* local_time = get_time();
-        log_record(local_time,log_fp);
-        log_record("\n",log_fp);
         html_fix(time,local_time,4);
         html_fix(time+4,local_time+5,2);
         html_fix(time+6,local_time+8,2);
-        printf("系统时间：%s，转换时间：%s\n",local_time,time);    
+        printf("抓取时间：%s\n",time);    
     }
     else if( argc == 2 ) //指定了抓取日期
     {
-        // // 内容抓取并保存
-        // log_record("---启动内容抓取----按指定日期抓取----------------\n",log_fp);
-        //    html_fix(time,argv+1,8);     
-        // printf("抓取日期：%s\n",time);        
+        // 内容抓取并保存
+        log_record("---启动内容抓取----按指定日期抓取----------------\n",log_fp);
+        strcat(time,argv[1]);     
+        printf("抓取时间：%s\n",time);         
     }
-    else{
+    else{   // 输入参数错误
         // 资源释放
         log_record("输入参数错误\n\n\n\n\n\n",log_fp);
         fclose(log_fp);
-        return 1;
-
+        printf("输入参数错误\n");
+        exit(1);
     }
 
     // 定义文件名
